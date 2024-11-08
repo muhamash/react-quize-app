b57b7b907c4651403b88820435c1367f12fd93e7
muhamsh <muhammad-ashraful@outlook.com>
2024-11-09 04:45:37 +0600

    route init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import './App.css'
-// import DashBoard from './components/admin/DashBoard'
-// import AddQuizCardPage from './page/AddQuizCardPage'
-import CreateQuiz from './page/CreateQuiz'
-// import RegistrationPage from './page/RegistrationPage'
-// import ResultPage from './page/ResultPage'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import AddQuizCardPage from './page/AddQuizCardPage';
+import CreateQuiz from './page/CreateQuiz';
+import DashBoard from './page/DashBoard';
+import IndexPage from './page/IndexPage';
+import LeaderBoard from './page/LeaderBoard';
+import Login from './page/Login';
+import QuizePage from './page/QuizePage';
+import RegistrationPage from './page/RegistrationPage';
+import ResultPage from './page/ResultPage';
 
 function App() {
   return (
-    <div>
-      {/* <AddQuizCardPage/> */ }
-      <CreateQuiz/>
-    </div>
-  )
+    <Routes>
+      <Route element={ <IndexPage /> } path="/" exact />
+      <Route element={ <QuizePage /> } path="/quizess" />
+      <Route element={ <ResultPage /> } path="/result" />
+      <Route element={ <LeaderBoard /> } path="/leaderBoard" />
+      <Route element={ <DashBoard /> } path="/dashBoard" />
+      <Route element={ <AddQuizCardPage/> } path="/addQuiz" />
+      <Route element={ <CreateQuiz /> } path="createQuiz" />
+      <Route element={ <RegistrationPage /> } path="/registration" />
+      <Route element={ <Login /> } path="/login" />
+    </Routes>
+  );
 }
 
 export default App
