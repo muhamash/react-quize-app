f933ffc5ace2aa76f3667895449a9478796f7dcd
muhamsh <muhammad-ashraful@outlook.com>
2024-11-11 01:58:46 +0600

    error found

--- src/components/Index/QuizCard.jsx
+++ src/components/Index/QuizCard.jsx
@@ -1,4 +1,3 @@
- 
 /* eslint-disable react/prop-types */
 import { motion, useInView } from 'framer-motion';
 import React from 'react';
@@ -12,35 +11,37 @@ export default function QuizCard ({quiz})
     // throw new Error( "ErrorComponent", "This is a test error" );
 
     return (
-        <motion.div
-            onClick={ () =>
-            {
-                console.log("card")
-            }}
-            ref={ ref }
-            initial={ { opacity: 0, y: 30 } }
-            animate={ isInView ? { opacity: 1, y: 0 } : {} }
-            exit={ { x: -300 } }
-            transition={ { duration: 0.6, ease: "easeOut" } }
-            className="rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow max-h-[450px] cursor-pointer group relative"
-        >
-            <div className="group-hover:scale-105 absolute transition-all text-white text-center top-1/2 -translate-y-1/2 px-4">
-                <h1 className="text-5xl" style={ { fontFamily: "Jaro" } }>{ quiz.title }</h1>
-                <p className="mt-2 text-lg">{quiz.description}</p>
-            </div>
-            {
-                loading && (
-                    <div className="w-full flex items-center justify-center p-3">
-                        <GridLoader
-                            color="#4f197f"
-                            margin={ 20 }
-                            size={ 30 }
-                            width={ 10 }
-                        />
-                    </div>
-                )
-            }
-            <img onLoad={ () => setLoading( false ) } src="./assets/backgrounds/5.jpg" alt="JavaScript Hoisting" className={ `w-full h-full object-cover rounded mb-4 transition-all ${loading ? 'hidden' : 'block'}` } />
-        </motion.div>
+        <div>
+            <motion.div
+                onClick={ () =>
+                {
+                    console.log( "card" )
+                } }
+                ref={ ref }
+                initial={ { opacity: 0, y: 30 } }
+                animate={ isInView ? { opacity: 1, y: 0 } : {} }
+                exit={ { x: -300 } }
+                transition={ { duration: 0.6, ease: "easeOut" } }
+                className="rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow max-h-[450px] cursor-pointer group relative"
+            >
+                <div className="group-hover:scale-105 absolute transition-all text-white text-center top-1/2 -translate-y-1/2 px-4">
+                    <h1 className="text-5xl" style={ { fontFamily: "Jaro" } }>{ quiz.title }</h1>
+                    <p className="mt-2 text-lg">{ quiz.description }</p>
+                </div>
+                {
+                    loading && (
+                        <div className="w-full flex items-center justify-center p-3">
+                            <GridLoader
+                                color="#4f197f"
+                                margin={ 20 }
+                                size={ 30 }
+                                width={ 10 }
+                            />
+                        </div>
+                    )
+                }
+                <img onLoad={ () => setLoading( false ) } src="./assets/backgrounds/5.jpg" alt="JavaScript Hoisting" className={ `w-full h-full object-cover rounded mb-4 transition-all ${loading ? 'hidden' : 'block'}` } />
+            </motion.div>
+        </div>
     );
 }
