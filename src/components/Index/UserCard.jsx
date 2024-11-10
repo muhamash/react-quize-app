a63bba9d67f087f0ed9eb559600ddfdd484125d7
muhamsh <muhammad-ashraful@outlook.com>
2024-11-10 15:50:30 +0600

    oka

--- src/components/Index/UserCard.jsx
+++ src/components/Index/UserCard.jsx
@@ -10,8 +10,8 @@ export default function UserCard ({userName})
         className="w-32 h-32 rounded-full border-4 border-primary mx-auto mb-4 object-cover" />
       <p className="text-xl text-gray-600">Welcome</p>
       <h2 className="text-4xl font-bold text-gray-700" style={ {
-          fontFamily: "Jaro"
+        fontFamily: "Jaro"
       } }>{ userName }</h2>
     </div>
-  )
+  );
 }
