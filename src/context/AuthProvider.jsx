247604e1f74a4fab954030dc3afa223ae4269e44
muhamsh <muhammad-ashraful@outlook.com>
2024-11-13 22:08:49 +0600

    moving for leader board

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,47 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import localforage from 'localforage';
+import React, { useEffect } from 'react';
 import { AuthContext } from '../context/index';
 
-export default function AuthProvider ( { children } )
-{
+export default function AuthProvider({ children }) {
     const [ auth, setAuth ] = React.useState( {} );
+
+    useEffect( () =>
+    {
+        const loadAuth = async () =>
+        {
+            const storedAuth = await localforage.getItem( 'auth' );
+            
+            console.log( storedAuth );
+            if ( storedAuth )
+            {
+                setAuth( storedAuth );
+            }
+        };
+        loadAuth();
+    }, [] );
+
+    useEffect( () =>
+    {
+        if ( auth?.authToken )
+        {
+            localforage.setItem( 'auth', auth );
+        } else
+        {
+            localforage.removeItem( 'auth' );
+        }
+    }, [ auth ] );
+
+    const logout = async () =>
+    {
+        setAuth( {} );
+        await localforage.removeItem( 'auth' );
+    };
+
     // console.log( auth );
 
     return (
-        <AuthContext.Provider value={ { auth, setAuth } }>
+        <AuthContext.Provider value={ { auth, setAuth, logout } }>
             { children }
         </AuthContext.Provider>
     );
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
- 
 import axios from 'axios';
+import localforage from 'localforage';
 import React from 'react';
 import { api } from '../components/api/localhost.js';
 import useAuth from '../hooks/useAuth.jsx';
@@ -45,6 +45,10 @@ const useAxios = () =>
                         const { accessToken } = response.data.data
                         console.log( 'new token', response );
                         setAuth( { ...auth, authToken: accessToken } );
+                        // mainRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+                        await localforage.setItem( 'auth', { ...auth, authToken: accessToken } );
+                        
                         mainRequest.headers.Authorization = `Bearer ${accessToken}`;
 
                         return axios( mainRequest )
