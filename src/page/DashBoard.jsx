b82d5911bd94308cd7f277e6777e76f23391f782
muhamsh <muhammad-ashraful@outlook.com>
2024-11-23 18:12:48 +0600

    admin ui

--- src/page/DashBoard.jsx
+++ src/page/DashBoard.jsx
@@ -191,6 +191,7 @@ export default function DashBoard() {
                     </div>
                 </div>
             </HelmetProvider>
+            
             { open && <QuizCardForm onClose={ handleClose } /> }
             
             { openQuestion && (
